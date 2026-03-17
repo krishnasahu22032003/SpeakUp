f8b294d34b2191e686f8bcf2adf5536c677f35b0
krishna sahu <krishna.sahu.work@gmail.com>
2026-03-17 20:14:27 +0530

    Authmiddlware added

---
+++ backend/src/middlewares/userAuthMiddleware.ts
@@ -0,0 +1,73 @@
+import type { Request, Response, NextFunction } from "express";
+import { ENV } from "../lib/ENV.js";
+import jwt from "jsonwebtoken";
+import { prisma } from "../lib/prisma.js";
+
+interface JwtPayload {
+  userId: number;
+  role: "USER" | "ADMIN";
+}
+
+if (!ENV.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
+
+const secret = ENV.JWT_SECRET;
+
+export async function AuthMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token =
+    req.cookies?.auth_token ||
+    req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Token is not present",
+    });
+  }
+
+  let decoded: JwtPayload;
+
+  try {
+    decoded = jwt.verify(token, secret) as JwtPayload;
+  } catch {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+      select: {
+        id: true,
+        role: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    req.user = {
+      id: user.id,
+      role: user.role,
+    };
+
+    next();
+  } catch (error) {
+    console.error("Auth Middleware Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+}
--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,9 +1,12 @@
+import "express";
+
 declare global {
-    namespace Express {
-        interface Request {
-            user?: {
-                id: string
-            }
-        }
+  namespace Express {
+    interface Request {
+      user?: {
+        id: number;
+        role:"USER" | "ADMIN"
+      };
     }
+  }
 }
