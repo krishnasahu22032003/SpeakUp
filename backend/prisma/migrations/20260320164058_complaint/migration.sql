56b4c8c8956b5a76535054388992756c96c6f9a0
krishna sahu <krishna.sahu.work@gmail.com>
2026-03-20 22:59:06 +0530

    create complaint code done

---
+++ backend/prisma/migrations/20260320164058_complaint/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE "Complaint" DROP CONSTRAINT "Complaint_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Complaint" ALTER COLUMN "userId" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Complaint" ADD CONSTRAINT "Complaint_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- backend/src/controllers/ComplaintController.ts
+++ backend/src/controllers/ComplaintController.ts
@@ -1,10 +1,57 @@
-import type{ Request , Response } from "express";
+import type { Request, Response } from "express";
 import { prisma } from "../lib/prisma.js";
+import { CreateComplaintSchema } from "../schemas/complaintSchema.js";
+import { v4 as uuidv4 } from 'uuid';
 
-export async function CreateComplaint (req : Request , res :Response) {
+export async function CreateComplaint(req: Request, res: Response) {
+
+    const ComplaintData = CreateComplaintSchema.safeParse(req.body);
+
+    if (!ComplaintData.success) {
+        return res.status(400).json({
+            success: false,
+            message: "invalid input",
+            error: ComplaintData.error.flatten()
+        })
+    };
+
+    const { title, description, longitude, latitude, image, type, location } = ComplaintData.data;
+
+    try {
+
+        const Complaint = await prisma.complaint.create({
+            data: {
+                title,
+                description,
+                ...(req.user && { userId: req.user.id }),
+                type,
+                ...(latitude !== undefined && { latitude }),
+                ...(longitude !== undefined && { longitude }),
+                ...(image !== undefined && { image }),
+                ...(location !== undefined && { location }),
+                complaintId: uuidv4()
+
+            }
+        });
+        if (!Complaint) {
+            return res.status(400).json({
+                success: false,
+                message: "Complaint not created"
+            })
+        };
+        return res.status(201).json({
+            success: true,
+            message: "Complaint created",
+            data:Complaint
+        })
 
 
+    } catch (error) {
 
-
-
+        console.error("Error while creating complaint:", error)
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error "
+        })
+    }
 }
