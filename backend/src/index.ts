b263fe8b7980cc6c721c0e0e39bf128dd8327856
krishna sahu <krishna.sahu.work@gmail.com>
2026-04-05 23:44:50 +0530

    cors added in backed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,10 @@ import cookieParser from 'cookie-parser';
 import UserRouter from './routes/UserRoutes.js';
 import AdminRouter from './routes/AdminRoute.js';
 import ComplaintRouter from './routes/ComplaintsRoute.js';
+import cors from "cors"; 
 
 const app = express();
+app.use(cors({origin:["http://localhost:5173"] , credentials:true }))
 app.use(express.json());
 app.use(cookieParser());
 
