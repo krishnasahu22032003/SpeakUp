e04eae9708e1adfeb850239716df7c50e803657d
krishna sahu <krishna.sahu.work@gmail.com>
2026-04-06 23:47:46 +0530

    zustand added and check user store created

---
+++ frontend/src/store/useAuthStore.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import { CheckUser } from "../lib/services/AuthService";
+
+type AuthService = {
+
+    user: any,
+    isAuth: boolean,
+    loading: boolean,
+    checkAuth: () => Promise<void>
+
+}
+
+export const CheckUserStore = create<AuthService>((set) => ({
+
+    user: null,
+    isAuth: false,
+    loading: true,
+
+    async checkAuth() {
+        try {
+            const user = await CheckUser();
+            set({ user, isAuth: true, loading: false });
+        } catch (error) {
+            set({ user: null, isAuth: false, loading: false });
+        }
+    },
+}));
