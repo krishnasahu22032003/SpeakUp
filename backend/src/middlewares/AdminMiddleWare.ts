2b41877b9f4ab681ff3501b6a7cb4ceabb249afb
krishna sahu <krishna.sahu.work@gmail.com>
2026-04-08 02:13:29 +0530

    adminmiddle ware added

--- backend/src/middlewares/AdminMiddleWare.ts
+++ backend/src/middlewares/AdminMiddleWare.ts
@@ -1,24 +1,65 @@
 import type { Request,Response,NextFunction } from "express";
+import { ENV } from "../lib/ENV.js";
+import jwt from "jsonwebtoken";
+import { prisma } from "../lib/prisma.js";
+
+interface JwtPayload {
+  userId: number;
+  role: "USER" | "ADMIN";
+}
 
 
-export function AdminMiddleware(req:Request,res:Response,next:NextFunction){
+if (!ENV.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
 
+const secret = ENV.JWT_SECRET;
 
-    if(!req.user || !req.user.id){
+export async function AdminMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token =
+    req.cookies?.["admin-token"] ||
+    req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
     return res.status(401).json({
-        success:false,
-         message: "Unauthorized",
-    })
-};
+      success: false,
+      message: "Admin token missing",
+    });
+  }
 
-if(req.user.role !== "ADMIN"){
-    return res.status(403).json({
-        success:false,
-        message:"Only Admins Allowed"
-    })
+  try {
+    const decoded = jwt.verify(token, secret) as JwtPayload;
+
+    const admin = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+      select: { id: true, role: true },
+    });
+
+    if (!admin) {
+      return res.status(401).json({
+        success: false,
+        message: "Admin not found",
+      });
+    }
+
+    if (admin.role !== "ADMIN") {
+      return res.status(403).json({
+        success: false,
+        message: "Not an admin",
+      });
+    }
+
+    req.user = admin;
+    next();
+
+  } catch {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid admin token",
+    });
+  }
 }
-
-next()
-
-}
-
--- backend/src/routes/AdminRoute.ts
+++ backend/src/routes/AdminRoute.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import { AdminSignup, GetAdminDetails } from '../controllers/AdminController.js';
+import { AdminSignIn, AdminSignup, GetAdminDetails } from '../controllers/AdminController.js';
 import { AdminMiddleware } from "../middlewares/AdminMiddleWare.js";
-import { UserAuthMiddleware } from "../middlewares/userAuthMiddleware.js";
 
 const AdminRouter = express.Router();
 
 AdminRouter.post('/signup', AdminSignup);
-AdminRouter.post('/signin', AdminSignup);
-AdminRouter.get('/me',UserAuthMiddleware,AdminMiddleware, GetAdminDetails);
+AdminRouter.post('/signin', AdminSignIn);
+AdminRouter.get('/me',AdminMiddleware, GetAdminDetails);
 
 export default AdminRouter
