331aed56724676579f46e4edb7f50c537b7b8609
krishna sahu <krishna.sahu.work@gmail.com>
2026-04-07 23:46:12 +0530

    Admin signin function added

--- frontend/src/lib/services/AdminAuthService.ts
+++ frontend/src/lib/services/AdminAuthService.ts
@@ -2,16 +2,23 @@ import API from "../../api/UserAPI";
 
 interface AdminDetails {
 
-    email : string ,
-    username : string ,
-    password: string 
+    email: string,
+    username?: string,
+    password: string
 }
 
-export function AdminSignUp(Admin : AdminDetails){
+export function AdminSignUp(Admin: AdminDetails) {
 
-return API<AdminDetails>("/admin/signup" , {
-    method:"POST",
-    body:JSON.stringify(Admin)
-})
+    return API<AdminDetails>("/admin/signup", {
+        method: "POST",
+        body: JSON.stringify(Admin)
+    })
+};
 
-}
+export function AdminSignIn(Admin: AdminDetails) {
+
+    return API<AdminDetails>("/admin/signin", {
+        method: "POST",
+        body: JSON.stringify(Admin)
+    })
+};
